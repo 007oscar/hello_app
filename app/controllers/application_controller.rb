b7769d0ff33426fc84baf9841647b72d395c4aa6
oscar <oscaralberto@gmail.com>
2017-03-26 23:08:59 +0000

    initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+    render html: "!!HOLA MUNDO!"
+  end
+  def goodbye
+    render html: 'good bye!'
+  end
+end
